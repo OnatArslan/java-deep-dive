e4053876595de0bb920688d84dde6186e032ca53
OnatArslan <onatarslan13@gmail.com>
2026-06-19 17:45:29 +0300

    Commit for changes

--- src/last_example/Printer.java
+++ src/last_example/Printer.java
@@ -1,4 +1,39 @@
 package last_example;
 
-public class Printer {
+public class Printer<T> {
+    private T value;
+
+    public Printer(T value) {
+        this.value = value;
+    }
+
+    public void print(){
+        System.out.println(this.value);
+    }
+
+    public static <T extends Integer> int getDouble(T value) {
+        return value * 2;
+    }
+
+
+    public static void main(String[] args) {
+        Printer<String> stringPrinter = new Printer<>("Onat Arslan");
+        stringPrinter.print();
+
+        Printer<Integer> intPrinter = new Printer<>(19);
+        intPrinter.print();
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
